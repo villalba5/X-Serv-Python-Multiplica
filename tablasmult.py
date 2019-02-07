515cb8ea59121c5e5e536d83d20dea2024fc4d8a
villalba5 <a.villalbacab@alumnos.urjc.es>
2019-02-07 11:38:55 +0100

    tablas de multiplicar del 1 al 10

---
+++ tablasmult.py
@@ -0,0 +1,5 @@
+for numeroprimero in range(1,11):
+	print("Tabla del",numeroprimero)
+	print("------------")
+	for numerosegundo in range(1,11):
+		print(numeroprimero, "por",numerosegundo,"es",numeroprimero*numerosegundo)
